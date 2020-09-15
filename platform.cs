42398c44fdb4fe2627333e40aaef2be4643ea8ef
jeslaa <60339624+jeslaa@users.noreply.github.com>
2020-09-15 13:14:41 +0200

    första

---
+++ platform.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Modellering
+{
+    public class Platform
+    {
+        public int width;
+        public bool falling;
+        public int x;
+        public int y;
+    }
+}
---
+++ platformprogram.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+namespace Modellering
+{
+     class platformprogram
+    {
+        static void Main(string[] arsg){
+        
+        Platform myPlatform = new Platform();
+
+        myPlatform.width = 10;
+        myPlatform.x = 5;
+        myPlatform.y = 2;
+        myPlatform.falling = true;
+
+     
+
+        }
+       
+
+    }
+}
